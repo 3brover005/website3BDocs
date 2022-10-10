f8548310c102a2f3f9c279433041b105cafab29d
james@3bforms.com <james@3bforms.com>
2022-10-10 14:23:39 +0300

    google tag

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,36 @@
+//Tawk Tawk
 var Tawk_API=Tawk_API||{}; 
 var Tawk_LoadStart = new Date();
+//Google Analytics
+window.dataLayer = window.dataLayer || [];
 
 export function init() {
   console.log('Page initiated!');
 
-  (function(){
-      var s1 = document.createElement("script");
-      var s0 = document.getElementsByTagName("script")[0];
-      s1.async = true;
-      s1.src = 'https://embed.tawk.to/633412ca54f06e12d8974a49/1ge1miplk';
-      s1.charset = 'UTF-8';
-      s1.setAttribute('crossorigin','*');
-      s0.parentNode.insertBefore(s1, s0);
-  })();
+    //Tawk Tawk`
+    (function(){
+        var s1 = document.createElement("script");
+        var s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/633412ca54f06e12d8974a49/1ge1miplk';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+
+    //Google Analytics
+    (function(){
+            var s1 = document.createElement("script");
+            var s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://www.googletagmanager.com/gtag/js?id=G-15Y9BG0QSZ';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s1.onload = function(){
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-15Y9BG0QSZ');
+            }
+            s0.parentNode.insertBefore(s1, s0);
+    })();
 }
